d06882522e264a807f377808f938321aea64a6b8
joeldantis <joeldantiv@gmail.com>
2025-06-14 03:37:12 +0530

    Try 1

--- main.py
+++ main.py
@@ -5,25 +5,44 @@ from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
 
+import subprocess
 
-class DemoExtension(Extension):
+class pmExtension(Extension):
 
     def __init__(self):
         super().__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+        self.subscribe(ItemEnterEvent, ItemEnterEventListener())
+
+
+class ItemEnterEventListener(EventListener):
+
+    def on_event(self, event, extension):
+        # event is instance of ItemEnterEvent
+
+        data = event.get_data()
+        if data == 'Shutdown':
+            subprocess.run(['poweroff'])
+        
+        elif data == 'Restart':
+            subprocess.run(['reboot'])
+        
+        elif data == 'Lock':
+            subprocess.run(['hyprlock'])
 
 
 class KeywordQueryEventListener(EventListener):
 
     def on_event(self, event, extension):
         items = []
-        for i in range(5):
+        states = ['Shutdown', 'Restart', 'Lock']
+        for i in states:
             items.append(ExtensionResultItem(icon='images/icon.png',
-                                             name='Item %s' % i,
-                                             description='Item description %s' % i,
+                                             name=i,
+                                             description="",
                                              on_enter=HideWindowAction()))
 
         return RenderResultListAction(items)
 
 if __name__ == '__main__':
-    DemoExtension().run()
+    pmExtension().run()
